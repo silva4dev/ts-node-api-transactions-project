ec1bf9c09dcf4c6337b757a7febaa087c94ccb32
silva4dev <web4dev.lucas@outlook.com>
2023-06-05 10:57:48 -0300

    inicial

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
-app.get("/hello", () => {
-  return "Hello world!";
-});
+app.get('/hello', () => {
+  return 'Hello world!'
+})
 
 app.listen({ port: 3333 }).then(() => {
-  console.log("HTTP Server Running!");
-});
+  console.log('HTTP Server Running!')
+})
