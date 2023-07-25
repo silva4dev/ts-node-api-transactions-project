fc445020f08e30cf8b5d5efdc731e74d85e5e3af
silva4dev <web4dev.lucas@outlook.com>
2023-07-25 20:05:09 -0300

    chore: change server

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import { app } from './app'
 import { env } from './env'
 
 app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
-  console.log('HTTP Server Running!')
+  console.log(`HTTP Server Running! ${env.PORT}`)
 })
